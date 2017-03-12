9b71ae9f632b68fe8ff5dfeaa6574fe6d32b54ea
Taihua Li <taihua.ray.li@gmail.com>
2017-03-11 22:29:31 -0600

    edited MAP, MAR, MAF

--- gutendex/books/evaluation/getMetrics.py
+++ gutendex/books/evaluation/getMetrics.py
@@ -6,6 +6,8 @@ Takes in two csv files:
 Outputs precision, recall and F-measure
 """
 
+import os
+
 def get(f1, f2 = 'Time_REL.csv'):
     def get_average(lst):
         return sum(lst)/float(len(lst))
@@ -13,11 +15,7 @@ def get(f1, f2 = 'Time_REL.csv'):
     groundTruth = open(f2, 'r').readlines() # each line represents a list of relevant docs
     retrieved = open(f1, 'r').readlines()   # each line represents a list of retrieved docs
 
-    all_precision = []
-    all_recall = []
-    all_f_measure = []
-
-    for i in range(len(groundTruth)): # number of queries
+    for i in range(1): # number of queries
         precision = []
         recall = []
         f_measure = []
@@ -42,19 +40,50 @@ def get(f1, f2 = 'Time_REL.csv'):
             temp_recall.append(r_j)
             temp_f_measure.append(f_j)
 
-        precision.append(get_average(temp_precision))
-        recall.append(get_average(temp_recall))
-        f_measure.append(get_average(temp_f_measure))
+        # store all precision, recall and f measure for one query
+        # list of lists [0] <- precision for query 1
+        precision.append(temp_precision)
+        recall.append(temp_recall)
+        f_measure.append(temp_f_measure)
 
-    all_precision.append(precision)
-    all_recall.append(recall)
-    all_f_measure(f_measure)
+    precision_k = []
+    recall_k = []
+    f_measure_k = []
 
-    output = open('Evals.csv', 'w')
+    k = len(precision[0])
 
-    for i in range(len(all_precision)):
-        output.write(str(i+1) + ',' + ','.join(str(j) for j in all_precision[i]))
-        output.write(str(i+1) + ',' + ','.join(str(j) for j in all_recall[i]))
-        output.write(str(i+1) + ',' + ','.join(str(j) for j in all_f_measure[i]))
+    for j in range(k):  # for each position
 
+        total_precision = 0
+        total_recall = 0
+        total_f_measure = 0
+
+        for i in range(len(precision)): # for each query
+            total_precision += precision[i][j]
+            total_recall += recall[i][j]
+            total_f_measure += f_measure[i][j]
+
+        precision_k.append(total_precision / len(precision))
+        recall_k.append(total_recall / len(recall))
+        f_measure_k.append(total_f_measure / len(f_measure))
+
+    file_dir = 'Evals_{}.csv'.format(f1.replace('.csv', ''))
+
+    output = open(file_dir, 'w')
+
+    output.write(','.join(str(j) for j in precision_k))
+    output.write('\n')
+    output.write(','.join(str(j) for j in recall_k))
+    output.write('\n')
+    output.write(','.join(str(j) for j in f_measure_k))
+    output.write('\n')
     output.close()
+
+
+if __name__ == '__main__':
+    files = os.listdir(os.getcwd())
+    for file in files:
+        if not file.startswith("__") and not file.endswith(".py") \
+                and file != 'Time_REL.csv' and not file.startswith("Evals_"):
+            get(file)
+
